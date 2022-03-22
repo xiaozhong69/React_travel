789420f17467bc3270f25988f16a48c2d16ea8b5
zzz <1401799140@qq.com>
2022-03-22 11:18:37 +0800

    初始化路由,内容-图片使用Link组件,引用钩子函数

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,30 +1,50 @@
-import React from 'react';
-import {Image,Typography} from 'antd';
+import React from "react";
+import { withRouter, RouteComponentProps,Link } from "react-router-dom";
+import { Image, Typography } from "antd";
 
-interface PropsType {
-    id: string | number;
-    size: "large" | "small";
-    imageSrc: string;
-    price: number | string;
-    title: string;
+interface PropsType extends RouteComponentProps {
+	id: string | number;
+	size: "large" | "small";
+	imageSrc: string;
+	price: number | string;
+	title: string;
 }
 
-export const ProductImage : React.FC<PropsType> = ({id,size,imageSrc,price,title})=>{
-    return (
-        <>
-            {
-                size === "large" ? (<Image src={imageSrc} height={285} width={490}/>)
-                : (<Image src={imageSrc} height={120} width={240}/>)
-            }
-            <div>
-                <Typography.Text type='secondary'>
-                    {title.slice(0,25)}
-                </Typography.Text>
-                <Typography.Text type='danger' strong>
-                    ￥ {price} 起
-                </Typography.Text>
+export const ProductImageComponent: React.FC<PropsType> = ({
+	id,
+	size,
+	imageSrc,
+	price,
+	title,
+    history,
+    location,
+    match
+}) => {
+	return (
+		<Link to={`detail/${id}`}>
+			{size === "large" ? (
+				<Image
+					src={imageSrc}
+					height={285}
+					width={490}
+				/>
+			) : (
+				<Image
+					src={imageSrc}
+					height={120}
+					width={240}
+				/>
+			)}
+			<div>
+				<Typography.Text type="secondary">
+					{title.slice(0, 25)}
+				</Typography.Text>
+				<Typography.Text type="danger" strong>
+					￥ {price} 起
+				</Typography.Text>
+			</div>
+		</Link>
+	);
+};
 
-            </div>
-        </>
-    )
-}
+export const ProductImage = withRouter(ProductImageComponent);
---
+++ src/pages/signIn/SignInPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const SignInPage : React.FC = ()=>{
+    return (
+        <h1>登录页面</h1>
+    )
+}
